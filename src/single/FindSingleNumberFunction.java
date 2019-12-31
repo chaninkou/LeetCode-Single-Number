ad20d5296d69dee04296c618abd44f3f8be77816
chaninkou <michaelkoulove@yahoo.com>
2019-12-30 16:22:07 -0800

    Great review on finding single appear number in an array using XOR.

--- src/single/FindSingleNumberFunction.java
+++ src/single/FindSingleNumberFunction.java
@@ -4,10 +4,12 @@ public class FindSingleNumberFunction {
 	public int singleNumber(int[] nums) {
 		int answer = 0;
 
+		// Go through the whole array
 		for (int i = 0; i != nums.length; i++) {
 			// answer = A ^ B ^ A ... = B
 			// A ^ A = 0
 			// 0 ^ B = B
+			// ^ = XOR
 			answer = answer ^ nums[i];
 		}
 
