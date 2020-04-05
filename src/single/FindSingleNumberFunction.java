d5c1f406f81cc5e6ef28a7359beaaa89e17f5ca1
chaninkou <michaelkoulove@yahoo.com>
2020-04-05 16:22:49 -0700

    Redid problem on using hashmap to find non duplicates or using XOR *

--- src/single/FindSingleNumberFunction.java
+++ src/single/FindSingleNumberFunction.java
@@ -5,37 +5,43 @@ import java.util.Map;
 import java.util.Set;
 
 public class FindSingleNumberFunction {
-	// O(n) time, fastest way to do it
-//	public int singleNumber(int[] nums) {
-//		int answer = 0;
-//
-//		// Go through the whole array
-//		for (int i = 0; i != nums.length; i++) {
-//			// answer = A ^ B ^ A ... = B
-//			// A ^ A = 0
-//			// 0 ^ B = B
-//			// ^ = XOR
-//			answer = answer ^ nums[i];
-//		}
-//
-//		return answer;
-//	}
+	// O(n) time, using bitwise fastest way to do it, without extra memory
+	public int singleNumber(int[] nums) {
+		int answer = 0;
+
+		// Go through the whole array
+		for (int i = 0; i != nums.length; i++) {
+			// answer = A ^ B ^ A ... = B
+			// A ^ A = 0
+			// 0 ^ B = B
+			// ^ = XOR
+			answer = answer ^ nums[i];
+		}
+
+		return answer;
+	}
 	
 	// My way of solving it, using hashmap to count, then check the hashmap to see which value is 1 and return that key
-    public int singleNumber(int[] nums) {
+    public int singleNumber1(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
         
         for(int num : nums){
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
         
-        Set<Map.Entry<Integer, Integer>> set = map.entrySet();
-        
-        for(Map.Entry<Integer, Integer> current : set){
-            if(current.getValue() == 1){
-                return current.getKey();
+        for(int key : map.keySet()){
+            if(map.get(key) == 1){
+                return key;
             }
         }
+        
+//        Set<Map.Entry<Integer, Integer>> set = map.entrySet();
+//        
+//        for(Map.Entry<Integer, Integer> current : set){
+//            if(current.getValue() == 1){
+//                return current.getKey();
+//            }
+//        }
         
         return 0;
     }
